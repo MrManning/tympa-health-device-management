dbef87e63432e02ba58f668e16fa5b51d8e03cdd
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2023-07-18 01:18:17 +0100

    run sql script on startup

--- api/src/db/setup.ts
+++ api/src/db/setup.ts
@@ -1,42 +1,53 @@
-
-import { Client } from 'ts-postgres';
-import config from '../config';
-import fs from 'fs';
-import path from 'path';
+import { Client } from "ts-postgres";
+import config from "../config";
+import fs from "fs";
+import path from "path";
 
 // db
 export const db = new Client({
-    // user: config.db.user,
-    host: config.db.host,
-    // password: config.db.password,
-    port: 5432
+  host: config.db.host,
+  port: 5432,
 });
-  
-const sql = fs.readFileSync(path.resolve(__dirname, './migrations/tbl_devices.sql')).toString();
+
+const sqlFile = fs.readFileSync(
+  path.resolve(__dirname, "./migrations/tbl_devices.sql"),
+  "utf-8"
+);
+
 export const createDatabase = async () => {
-    await db.connect();                       // gets connection
+  await db.connect();
 
-    const res = await db.query(`SELECT datname FROM db_catalog.db_database WHERE datname = '${config.db.database}'`);
+  const results = await db.query(
+    `SELECT FROM pg_database WHERE datname = '${config.db.database}'`
+  );
+  if (results.rows.length === 0) {
+    console.log(`${config.db.database} database not found, creating it.`);
+    await db.query(`CREATE DATABASE "${config.db.database}";`);
+  } else {
+    console.log(`${config.db.database} database exists.`);
+  }
 
-    if (res.rows.length === 0) {
-        console.log(`${config.db.database} database not found, creating it.`);
-        await db.query(`CREATE DATABASE "${config.db.database}";`);
-        console.log(`created database ${config.db.database}`);
-        console.log('creating table');
+  const queries = sqlQueries(sqlFile);
+  await processSqlQueries(db, queries);
+  await db.end();
+};
 
-        try {
-            await db.query(sql);
-        } catch(error) {
-            console.log('error: ', error);
-        }
-    } else {
-        console.log(`${config.db.database} database exists.`);
-    }
+const sqlQueries = (file: string): string[] => {
+  return file
+    .replace(/(\r\n|\n|\r)/gm, " ")
+    .replace(/\s+/g, " ")
+    .split(";")
+    .map((query) => query.trim())
+    .filter(Boolean);
+};
 
-    await db.end();
-}
+const processSqlQueries = async (db: Client, queries: string[]) => {
+  for (const query of queries) {
+    await db.query(query);
+  }
+};
 
 export default {
-    db,
-    createDatabase,
-}
+  db,
+  createDatabase,
+};
